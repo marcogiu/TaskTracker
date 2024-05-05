0425778e1c8fd9e25d8d99327a33d61620bc2c6a
marcogiu <48352400+marcogiu@users.noreply.github.com>
2024-05-05 16:11:11 +0200

    fix server main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,15 @@ import { environments } from 'config/enviroments';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [environments.corsOrigin],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
