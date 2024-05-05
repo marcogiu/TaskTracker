3b380c846cb641746644f44fe182e10b3877f60c
marcogiu <48352400+marcogiu@users.noreply.github.com>
2024-05-05 16:00:11 +0200

    update

--- client/src/service/apiService.ts
+++ client/src/service/apiService.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://task-tracker-server-chi.vercel.app/",
   prepareHeaders: (headers) => {
     const userInfoString = localStorage.getItem("userInfo");
     if (userInfoString) {
